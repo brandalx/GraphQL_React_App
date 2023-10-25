abe412c53303680d2807c4bccbc81a58dcd3bffa
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-25 08:13:48 +0400

    feat: root resolver

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
-const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
-const cors = require("cors");
-const schema = require("./schema.js");
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
+const schema = require('./schema.js');
+const users = [];
 const app = express();
 app.use(cors());
+
+const root = {
+  getAllUsers: () => {
+    return users;
+  },
+
+  getUsers: ({ id }) => {
+    return users.find((users) => users.id == id);
+  },
+};
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHTTP({
     graphiql: true,
     schema,
-  })
+    rootValue: root,
+  }),
 );
-app.listen(5001, () => console.log("Server running"));
+app.listen(5001, () => console.log('Server running'));
