2e8bbbe7a67141d02e1bd97bb487b99d23a73d0f
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-25 08:55:49 +0400

    feat: form for users

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
   return (
     <div className="App">
-      <h1>App init</h1>
+      <div>
+        <form>
+          <input type="text" />
+          <input type="text" />
+          <div className="btns">
+            <button>Create</button>
+            <button>Recive</button>
+          </div>
+        </form>
+        <div>
+          {users.map((user) => (
+            <div className="user">
+              {user.id}. {user.username} {user.age}{" "}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
