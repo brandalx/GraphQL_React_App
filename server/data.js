e570b9865a5d39e5498dc6049da62c0ff059d0e6
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-25 08:34:59 +0400

    feat: fake data with posts update

--- server/data.js
+++ server/data.js
@@ -1,29 +1,92 @@
 const users = [
-  { id: 1, username: 'John', age: 23 },
+  {
+    id: 1,
+    username: 'John',
+    age: 23,
+    posts: [
+      { id: 1, title: 'Some new title', content: 'This is some content' },
+    ],
+  },
   { id: 2, username: 'Alice', age: 30 },
-  { id: 3, username: 'Bob', age: 28 },
+  {
+    id: 3,
+    username: 'Bob',
+    age: 28,
+    posts: [{ id: 2, title: 'Another title', content: 'More content here' }],
+  },
   { id: 4, username: 'Ella', age: 35 },
   { id: 5, username: 'David', age: 29 },
-  { id: 6, username: 'Sophia', age: 27 },
+  {
+    id: 6,
+    username: 'Sophia',
+    age: 27,
+    posts: [
+      {
+        id: 3,
+        title: "Sophia's thoughts",
+        content: "This is Sophia's content",
+      },
+    ],
+  },
   { id: 7, username: 'Oliver', age: 22 },
-  { id: 8, username: 'Emma', age: 25 },
+  {
+    id: 8,
+    username: 'Emma',
+    age: 25,
+    posts: [{ id: 4, title: "Emma's post", content: 'Content by Emma' }],
+  },
   { id: 9, username: 'Liam', age: 26 },
   { id: 10, username: 'Grace', age: 32 },
-  { id: 11, username: 'Lucas', age: 31 },
+  {
+    id: 11,
+    username: 'Lucas',
+    age: 31,
+    posts: [{ id: 5, title: "Lucas's title", content: "Lucas's story" }],
+  },
   { id: 12, username: 'Mia', age: 24 },
   { id: 13, username: 'Noah', age: 27 },
-  { id: 14, username: 'Ava', age: 29 },
+  {
+    id: 14,
+    username: 'Ava',
+    age: 29,
+    posts: [{ id: 6, title: "Ava's diary", content: "Ava's day today" }],
+  },
   { id: 15, username: 'William', age: 34 },
   { id: 16, username: 'Emily', age: 26 },
   { id: 17, username: 'James', age: 33 },
-  { id: 18, username: 'Charlotte', age: 28 },
+  {
+    id: 18,
+    username: 'Charlotte',
+    age: 28,
+    posts: [
+      { id: 7, title: "Charlotte's musings", content: 'What Charlotte thinks' },
+    ],
+  },
   { id: 19, username: 'Benjamin', age: 30 },
   { id: 20, username: 'Harper', age: 25 },
   { id: 21, username: 'Henry', age: 29 },
   { id: 22, username: 'Evelyn', age: 27 },
-  { id: 23, username: 'Michael', age: 32 },
+  {
+    id: 23,
+    username: 'Michael',
+    age: 32,
+    posts: [
+      {
+        id: 8,
+        title: "Michael's adventures",
+        content: 'Adventures of Michael',
+      },
+    ],
+  },
   { id: 24, username: 'Abigail', age: 31 },
-  { id: 25, username: 'Alexander', age: 33 },
+  {
+    id: 25,
+    username: 'Alexander',
+    age: 33,
+    posts: [
+      { id: 9, title: "Alexander's tales", content: 'Tales by Alexander' },
+    ],
+  },
 ];
 
 module.exports = users;
