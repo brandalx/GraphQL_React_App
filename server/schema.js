1c11fc98d45b942cfcc39e455664092a4fa5c2f2
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-25 07:59:33 +0400

    feat: basic grapql schema

--- server/schema.js
+++ server/schema.js
@@ -1,6 +1,16 @@
 const { buildSchema } = require("graphql");
 const schema = buildSchema(`
-
+type User {
+    id: ID
+    username: String
+    age: Int
+    posts: [Post]
+}
+type Post {
+    id: ID
+    title: String
+    content : String
+}
 `);
 
 module.exports = schema;
