4c68c77ad61a0671bd8b29a1f942c93450f12466
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-25 07:57:38 +0400

    feat: grapql schema init

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const cors = require("cors");
-
+const schema = require("./schema.js");
 const app = express();
 app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
     graphiql: true,
+    schema,
   })
 );
 app.listen(5001, () => console.log("Server running"));
