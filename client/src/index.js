4e485426db74261bcc8d5e5476a0e4aef348823d
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-25 08:51:31 +0400

    feat: apollo config, provider, cache.

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  url: "http://localhost:5001/graphql",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
